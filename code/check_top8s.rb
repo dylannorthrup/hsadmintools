4f46aae6a76647655ee0552a42ff9151a4ee037f
Dylan Northrup <github@doc-x.net>
2019-06-09 10:20:29 -0700

    Adding support for single elim tournaments

--- code/check_top8s.rb
+++ code/check_top8s.rb
@@ -7,10 +7,16 @@ require 'open-uri'
 require 'json'
 require 'mysql'
 
+def pdebug(msg="")
+  return unless @DEBUG
+  puts("DEBUG: #{msg}<br>\n")
+end
 
 @top_x = 8
 
 @players = Hash.new(0)
+@DEBUG = false
+@DEBUG = true if $0.match(/ct8.rb$/)
 
 puts "Content-type: text/html; charset=UTF-8"
 puts ""
@@ -19,8 +25,6 @@ puts "<head>"
 puts "<title>Top #{@top_x} Finishers</title>"
 puts "</head>"
 puts "<body>"
-#checkmark = "\u2713"
-#puts checkmark.encode('utf-8')
 
 # Give a round number and get the results from that round
 def get_standings(bracket_id=nil)
@@ -32,14 +36,16 @@ def get_standings(bracket_id=nil)
   row = results.fetch_row
   # If we didn't get a result, go ahead and grab it and cache it in the DB
   if row.nil? 
+    pdebug("Did not find cached info for #{bracket_id}")
     bracket_url = "https://api.battlefy.com/stages/#{bracket_id}/matches"
     #bracket_url = "https://dtmwra1jsgyb0.cloudfront.net/stages/#{bracket_id}/rounds/#{fr}/standings"
-#    puts "Full URL: #{bracket_url}"
+    pdebug "Full URL: #{bracket_url}"
     raw_json = open(bracket_url, {ssl_verify_mode: 0}).read
     query = "INSERT INTO cached_standings (bracket_id, json_blob) values ('#{bracket_id}', '#{Mysql.escape_string(raw_json)}')"
 #    puts "GOING TO DO THIS: #{query}"
     @con.query(query)
   else
+    pdebug("Using cached info for #{bracket_id}")
     # If we got a result, use that
     raw_json = row[0]
   end
@@ -69,10 +75,19 @@ def get_tournament_ids
   return rows
 end
 
-def process_json_data(dj=nil)
+def process_json_data(dj=nil, type=nil)
   return if dj.nil?
-  dj.each do |p|
-    return if p["matchNumber"] > 4
+  return if type.nil?
+  # Take dj. Grab the last 7 elements (in case we're single elim, we're
+  # just interested in the last 7 events), then look at the first four
+  # of the resultant list (the round of 8) and get the folks from there
+  dj.last(7).first(4).each do |p|
+    # Double check that if we say we are in a single elim
+    # tournament, we actually ARE a single elim tournament
+    if type == 'single' then
+      return if p["matchType"].nil?
+      return unless p["matchType"] == 'winner'
+    end
     @players[p['top']['team']['name']] += 1
     @players[p['bottom']['team']['name']] += 1
   end
@@ -80,12 +95,12 @@ end
 
 def get_b_ids_from_t_ids(tids=nil)
   return if tids.nil?
-  b_ids = Array.new
+  b_ids = Hash.new
   #@tournament_hash.each_key do |t|
   tids.each do |tid|
     t = tid[0]
     t_url = "https://api.battlefy.com/tournaments/#{t}"
-#    puts "Retrieving info from #{t_url}"
+    pdebug "Retrieving info from #{t_url}"
     t_json = open(t_url, {ssl_verify_mode: 0}).read
 
     begin
@@ -96,8 +111,13 @@ def get_b_ids_from_t_ids(tids=nil)
     end
 
     unless t_data['stageIDs'][1].nil?
-#      puts " - Adding #{t_data['stageIDs'][1]} to b_ids"
-      b_ids.push(t_data['stageIDs'][1])
+      # This should be a swiss tournament since it has two brackets 
+      # (presumably swiss, then top 8)
+      pdebug " - Adding swiss event #{t_data['stageIDs'][1]} to b_ids"
+      b_ids[t_data['stageIDs'][1]] = 'swiss'
+    else
+      pdebug " - Adding possible single elim event #{t_data['stageIDs'][0]} to b_ids"
+      b_ids[t_data['stageIDs'][0]] = 'single'
     end
   end
   return b_ids
@@ -107,16 +127,16 @@ end
 t_ids = get_tournament_ids
 bracket_ids = get_b_ids_from_t_ids(t_ids)
 
-bracket_ids.each do |bid|
+bracket_ids.each_pair do |bid, type|
 #  puts "<pre>#{bid}</pre>"
 #  if b[1].nil?
 #    final_round=8
 #  else
 #    final_round=b[1]
 #  end
-#  puts "<li> Working on #{bid}"
+  puts "<li> Working on #{bid} (#{type})"
   data_json = get_standings(bid)
-  process_json_data(dj=data_json)
+  process_json_data(data_json, type)
 end
 #puts "</ul>"
 
