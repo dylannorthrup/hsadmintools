ac3744b5f071b615b851538b5bdf512e0a8c2a5f
Dylan Northrup <github@doc-x.net>
2019-09-23 08:23:55 -0700

    Making this create a full file

--- code/transform-tournament-urls.sh
+++ code/transform-tournament-urls.sh
@@ -2,4 +2,8 @@
 #
 # cat file into this script and it should output the transformed but ready to read into the match_status script
 
+echo "#!/usr/bin/ruby -w"
+echo ""
+echo "@tournament_hash = {"
 sed -e 's/info.*/stage/' | awk -F/ '{print "  \"" $(NF-1) "\" => \"" $0 "\","}'
+echo "}"
