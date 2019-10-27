cfd5d19801045c05b302c78e86c1b13942e6905f
Dylan Northrup <github@doc-x.net>
2019-10-27 12:25:36 -0700

    Updating to pass in variables. Need to use those variables in the actual logic soon-ish

--- code/ct8.rb
+++ code/ct8.rb
@@ -6,14 +6,21 @@
 # Read in utility methods and list of tournaments for current season
 $: << "/home/docxstudios/web/hs/code"
 if $0.match(/ct8.rb$/) then
-  @DEBUG = true 
   require "hsm"
+  @DEBUG = true 
 else
   require "hs_methods"
 end
 require "tournament_urls"
 
+# Uncomment these and set them appropriately if you want to see what's
+# giong on with a specific player or bracket
+#@tracked_player = 'TechnoGoose#2886'
+#@tracked_bracket = '5d999f3eee6bff69e6e49485|5da0a0bbb2f36e17303a97b9'
+
 @top_x = 8
+@threshold = 5
+@threshold_text = 'five'
 
 @players = Hash.new(0)
 @tournament_placements = Hash.new()
@@ -27,9 +34,11 @@ puts "<title>Top #{@top_x} Finishers</title>"
 puts "</head>"
 puts "<body>"
 
-puts "<h1> Players who've made Top #{@top_x} five or more times</h1>"
+puts "<h1> Players who've made Top #{@top_x} #{@threshold_text} or more times</h1>"
 puts "Data last refreshed at <tt>#{Time.now.utc.to_s}</tt><p>"
 puts ""
+
+pdebug "FYI, we're tracking player #{@tracked_player}"
 
 @con = get_db_con
 
