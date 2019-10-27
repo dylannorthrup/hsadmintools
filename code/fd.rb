851b33a94f99657bb0688c38f9d4b20d1d28c342
Dylan Northrup <github@doc-x.net>
2019-10-27 12:27:01 -0700

    Updating to use hsm; Adding easy debug note; converting over to use @output; updating to new dupe finding code

--- code/fd.rb
+++ code/fd.rb
@@ -3,29 +3,41 @@
 
 # Read in utility methods and list of tournaments for current season
 $: << "/home/docxstudios/web/hs/code"
-require "hs_methods"
+# If we're using the debug version, set debug and alternate form url
+if $0.match(/fd.rb$/) then
+  require "hsm"
+  @DEBUG = true if $0.match(/fd.rb$/)
+  @form_url = 'http://doc-x.net/hs/fd.html'
+else
+  require "hs_methods"
+end
 require "tournament_urls"
 
 @cgi = CGI.new
 params = @cgi.params
 
-@DEBUG = true if $0.match(/fd.rb$/)
+@output = ''
+@skip_match_data_stuff = true
 @dup_prefix = 'zzz__'
-@target_url = 'http://doc-x.net/hs/find_dupes.html'
+@form_url = 'http://doc-x.net/hs/find_dupes.html'
+@output = ""
 
 # If you want to test this, you can uncomment these and put in valid bracket_ids
-#params['b1'] = '000000000000000000000000'
-#params['b2'] = '000000000000000000000000'
+#params['b1'] = '5d98be246313cd683c69ac92'
+#params['b2'] = '5d98864aee6bff69e6e48f3d'
+#
+# Or, you can do the following (for the same data):
+# companion> echo 'b1=5d98be246313cd683c69ac92&b2=5d98864aee6bff69e6e48f3d' | ./fd.rb
 
 if params.empty? then
-  bail_and_redirect(target=@target_url)
+  bail_and_redirect(target=@form_url)
 end
 if params['b1'].nil? and pramas['b2'].nil?
-  bail_and_redirect(target=@target_url)
+  bail_and_redirect(target=@form_url)
 end
 
-puts "Content-type: text/plain; charset=UTF-8"
-puts ""
+@output.concat "Content-type: text/plain; charset=UTF-8\n"
+@output.concat "\n"
 
 pdebug "Params"
 pdebug "#{params}"
@@ -33,12 +45,20 @@ pdebug "#{params}"
 b1 = params['b1'][0]
 b2 = params['b2'][0]
 
+# Make sure our bracket data isn't empty
 if b1.nil? or b2.nil? then
-  puts "Something weird happened. Try refreshing your browser. Or yell at Dylan"
-  puts "DEBUG: b1='#{b1}' b2='#{b2}'"
+  @output.concat "Something weird happened. Try refreshing your browser. Or yell at Dylan\n"
+  @output.concat "DEBUG: b1='#{b1}' b2='#{b2}'\n"
 end
 
-# This was short-circuited previously. DEBUG WHY THIS IS
+# Make sure we're not comparing a bracket against itself
+if b1 == b2 then
+  @output.concat "Your brackets are identical. Probably a copy/paste error. Try again."
+  puts @output
+  exit
+end
+
+# Validate that our match data is well formed.
 if bogus_match_data(b1) then
   tell_em_dano(b1)
 end
@@ -46,28 +66,29 @@ if bogus_match_data(b2) then
   tell_em_dano(b2)
 end
 
-# 24 hex characters
-t1_hash = b1
-t2_hash = b2
+# For each bracket, get the list of active users in that bracket
+d1_users = get_user_list(b1, true)
+d2_users = get_user_list(b2, true)
 
-d1_users = get_user_list(b1)
-d2_users = get_user_list(b2)
+# Uncomment this for capturing interesting JSON data.
+#@out_dir = '/home/docxstudios/web/hs/code/json_examples'
+#d1_json = get_active_round_json_data(b1, true)
+#fqfn = "#{@out_dir}/#{b1}-all-matches.json"
+#File.open(fqfn, 'w') { |file| file.write(d1_json.to_json) }
+#d2_json = get_active_round_json_data(b2, true)
+#fqfn = "#{@out_dir}/#{b2}-all-matches.json"
+#File.open(fqfn, 'w') { |file| file.write(d2_json.to_json) }
 
-puts "========== FOR SINGLE ELIM TOURNAMENTS ================================ "
-puts "If the single elim tournament is the newest tournament, you should also "
-puts "drop players with '#{@dup_prefix}' in front of their names since they "
-puts "played, but lost quickly in the new single elim tournament"
-puts "========== FOR SINGLE ELIM TOURNAMENTS ================================ "
-puts ""
-puts "Comparing user lists"
-puts "+++++++++++++++++++++++++"
-d1_users.each do |u|
-  if d2_users.include?(u) then
-    puts "* Double dipper: #{u}"
-  end
+# Find out which tournament was most recent tournament (so we can ignore whether
+# or not the player is 'still_winning' because they shouldn't be in this new
+# tournament if they're 'still_winning' in the old one).
+d1_tournament_order = get_tournament_order_by_bid(b1)
+d2_tournament_order = get_tournament_order_by_bid(b2)
+
+if d1_tournament_order > d2_tournament_order
+  @output.concat find_double_dippers(d1_users, d2_users)
+else
+  @output.concat find_double_dippers(d2_users, d1_users)
 end
 
-puts "+++++++++++++++++++++++++"
-puts "Comparison complete"
-
-
+puts @output
--- code/find_dupes.rb
+++ code/find_dupes.rb
@@ -1,67 +1,64 @@
 #!/usr/bin/env ruby
 #
 
-require 'open-uri'
-require 'json'
-require 'cgi'
+# Read in utility methods and list of tournaments for current season
+$: << "/home/docxstudios/web/hs/code"
+# If we're using the debug version, set debug and alternate form url
+if $0.match(/fd.rb$/) then
+  require "hsm"
+  @DEBUG = true if $0.match(/fd.rb$/)
+  @form_url = 'http://doc-x.net/hs/fd.html'
+else
+  require "hs_methods"
+end
+require "tournament_urls"
 
 @cgi = CGI.new
 params = @cgi.params
 
-@DEBG=false
-@DEBUG = true if $0.match(/fd.rb$/)
+@output = ''
+@skip_match_data_stuff = true
+@dup_prefix = 'zzz__'
+@form_url = 'http://doc-x.net/hs/find_dupes.html'
+@output = ""
 
-def pdebug(msg="")
-  return unless @DEBUG
-  puts("DEBUG: #{msg}<br>\n")
-end
-
-def bail_and_redirect
-  target_url = 'http://doc-x.net/hs/find_dupes.html'
-  @cgi.out( "status" => "REDIRECT", "Location" => target_url, "type" => "text/html") {
-    "Redirecting to data input page: #{target_url}\n"
-  }
-  exit
-end
-
-def bogus_match_data(bid=nil?)
-  ### Short Circuit
-  return false
-  ### Short Circuit
-  return true if bid.nil?
-  return false if bid.match(/^[a-f0-9]{24}$/)
-  return true
-end
-
-def tell_em_dano(bid=nil)
-  puts "Provided bracket ID (#{b1}) did not match pattern. Hit the back button and try again."
-  exit
-end
-
-#params['b1'] = '000000000000000000000000'
-#params['b2'] = '000000000000000000000000'
+# If you want to test this, you can uncomment these and put in valid bracket_ids
+#params['b1'] = '5d98be246313cd683c69ac92'
+#params['b2'] = '5d98864aee6bff69e6e48f3d'
+#
+# Or, you can do the following (for the same data):
+# companion> echo 'b1=5d98be246313cd683c69ac92&b2=5d98864aee6bff69e6e48f3d' | ./fd.rb
 
 if params.empty? then
-  bail_and_redirect
+  bail_and_redirect(target=@form_url)
 end
 if params['b1'].nil? and pramas['b2'].nil?
-  bail_and_redirect
+  bail_and_redirect(target=@form_url)
 end
 
-puts "Content-type: text/plain; charset=UTF-8"
-puts ""
+@output.concat "Content-type: text/plain; charset=UTF-8\n"
+@output.concat "\n"
 
-puts "Params"
-puts "#{params}"
+pdebug "Params"
+pdebug "#{params}"
 
 b1 = params['b1'][0]
 b2 = params['b2'][0]
 
+# Make sure our bracket data isn't empty
 if b1.nil? or b2.nil? then
-  puts "Something weird happened. Try refreshing your browser. Or yell at Dylan"
-  puts "DEBUG: b1='#{b1}' b2='#{b2}'"
+  @output.concat "Something weird happened. Try refreshing your browser. Or yell at Dylan\n"
+  @output.concat "DEBUG: b1='#{b1}' b2='#{b2}'\n"
 end
 
+# Make sure we're not comparing a bracket against itself
+if b1 == b2 then
+  @output.concat "Your brackets are identical. Probably a copy/paste error. Try again."
+  puts @output
+  exit
+end
+
+# Validate that our match data is well formed.
 if bogus_match_data(b1) then
   tell_em_dano(b1)
 end
@@ -69,145 +66,29 @@ if bogus_match_data(b2) then
   tell_em_dano(b2)
 end
 
-@base_cf_url = 'https://dtmwra1jsgyb0.cloudfront.net/stages'
-# 24 hex characters
-t1_hash = b1
-t2_hash = b2
+# For each bracket, get the list of active users in that bracket
+d1_users = get_user_list(b1, true)
+d2_users = get_user_list(b2, true)
 
-# Give a round number and get the results from that round
-def get_round(round=nil, tourney_url=nil)
-  return if round.nil?
-  return if tourney_url.nil?
-  full_url = "#{tourney_url}/#{round}/matches"
-  pdebug("Full URL for this round's JSON: #{full_url}<br>\n")
-  raw_json = open(full_url, {ssl_verify_mode: 0}).read
+# Uncomment this for capturing interesting JSON data.
+#@out_dir = '/home/docxstudios/web/hs/code/json_examples'
+#d1_json = get_active_round_json_data(b1, true)
+#fqfn = "#{@out_dir}/#{b1}-all-matches.json"
+#File.open(fqfn, 'w') { |file| file.write(d1_json.to_json) }
+#d2_json = get_active_round_json_data(b2, true)
+#fqfn = "#{@out_dir}/#{b2}-all-matches.json"
+#File.open(fqfn, 'w') { |file| file.write(d2_json.to_json) }
 
-  begin
-    j_data = JSON.parse(raw_json)
-  rescue JSON::ParserError, Encoding::InvalidByteSequenceError => e
-    puts("Had problem parsing #{path}: #{e}\n")
-    return Hash.new
-  end
-  return j_data
+# Find out which tournament was most recent tournament (so we can ignore whether
+# or not the player is 'still_winning' because they shouldn't be in this new
+# tournament if they're 'still_winning' in the old one).
+d1_tournament_order = get_tournament_order_by_bid(b1)
+d2_tournament_order = get_tournament_order_by_bid(b2)
+
+if d1_tournament_order > d2_tournament_order
+  @output.concat find_double_dippers(d1_users, d2_users)
+else
+  @output.concat find_double_dippers(d2_users, d1_users)
 end
 
-
-def get_single_elim_data(tourney_url)
-  return if tourney_url.nil?
-  @tournament_type = 'single_elim'
-  data_json = Array.new
-  1.upto(8) do |round|
-    pdebug("Getting single_elim round data for #{round}")
-    new_data = get_round(round, tourney_url)
-    data_json.concat(new_data) unless new_data.nil?
-  end
-  return data_json
-end
-
-def extract_json_data(data_json=nil, current_round=nil)
-  return if data_json.nil?
-  return if current_round.nil?
-  @active_round = current_round
-  return data_json
-end
-
-
-
-# Iterate through the rounds from top down until you find a round that has matches
-def find_active_round(t_url=nil)
-  8.downto(1) do |current_round|
-    data_json = get_round(current_round, t_url)
-    if data_json.length() > 0 then
-      # Check to see if we're in a swiss or single-elim match
-      pdebug("JSON has #{data_json.length()} elements")
-      if data_json.length() == 1 then
-        pdebug("We only have 1 event which seems suspicious. Going to assume this is Single Elim")
-        #data_json = get_round(round=1, tourney_url=t_url)
-        data_json = get_single_elim_data(t_url)
-        return extract_json_data(data_json, 1)
-      else
-        return extract_json_data(data_json, current_round)
-      end
-    end
-  end
-  puts("Went through all rounds and did not find matches. Seems bad, dawg.\n")
-  exit
-end
-
-# Print user name (and ready status if they aren't ready)
-def print_user(user=nil)
-  return if user.nil?
-  if @tournament_type == 'swiss' then
-    name = user['name']
-  else
-    if user['team'].nil?
-      name = 'team_not_defined'
-    else
-      name = user['team']['name']
-    end
-  end
-  return if name.nil?
-  if user['readyAt'].nil?
-    name += " (NOT-READY)"
-  end
-  return name
-end
-
-def get_json_data(hash=nil?)
-  return if hash.nil?
-  url = "#{@base_cf_url}/#{hash}/rounds"
-  puts "Round URL: #{url}"
-  data_json = find_active_round(t_url=url)
-  return data_json
-end
-
-def get_users(dj=nil?)
-  return if dj.nil?
-  r_ary = []
-  pdebug("Getting users from #{@tournament_type} tournament")
-  dj.each do |f|
-    if @tournament_type == 'swiss' then
-      r_ary.push(f['top']['name']) unless f['top']['name'].nil?
-      r_ary.push(f['bottom']['name']) unless f['bottom']['name'].nil?
-    else
-      # For single-elim, we only add players who are active (i.e. have a match
-      # without a 'winner' attribute)
-      unless f['top']['team'].nil? then
-        if f['top']['winner'].nil? then
-          r_ary.push(f['top']['team']['name']) unless f['top']['team']['name'].nil?
-        end
-      end
-      unless f['bottom']['team'].nil? then
-        if f['bottom']['winner'].nil? then
-          r_ary.push(f['bottom']['team']['name']) unless f['bottom']['team']['name'].nil?
-        end
-      end
-    end
-  end
-  return r_ary
-end
-
-def get_user_list(bid=nil)
-  puts "Getting data for tournament '#{bid}'"
-  @tournament_type = 'swiss'
-  dj = get_json_data(bid)
-  users = get_users(dj)
-  puts "Total of #{users.length} users in bracket #{bid}"
-end
-
-d1_users = get_user_list(b1)
-d2_users = get_user_list(b2)
-
-puts "Comparing user lists"
-puts "+++++++++++++++++++++++++"
-d1_users.each do |u|
-  u1 = u['name']
-  if d2_users.include?(u) then
-    puts "* Double dipper: #{u}"
-  end
-end
-
-puts "+++++++++++++++++++++++++"
-puts "Comparison complete"
-
-
+puts @output
